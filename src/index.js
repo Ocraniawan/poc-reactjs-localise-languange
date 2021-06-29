6924a25570886111ba2965f0940c7da0d293756d
ocraniawan patattan <Ocraniawanpatattan@gmail.com>
2021-06-29 17:41:56 +0700

    install i18 package for internationalization language
    - install package i18next react-i18next i18next-http-backend i18next-browser-languagedetector
    - create App.js files and update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-const HelloWorld = () => {
-    return (
-        <h1>
-            Hello World
-        </h1>
-    );
-}
-
-ReactDOM.render(<HelloWorld />, document.getElementById("root"));
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+    , document.getElementById("root"));
 
 // https://dev.to/deadwing7x/setup-a-react-app-with-webpack-and-babel-4o3k
