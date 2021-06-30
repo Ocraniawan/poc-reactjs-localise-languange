2112e70599331770e80f25056d76143ff6b656bb
ocraniawan patattan <Ocraniawanpatattan@gmail.com>
2021-06-30 11:42:32 +0700

    update app js file to use i18n
    - implement react-i18next in app.js file
    
    thank you!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Trans, useTranslation } from 'react-i18next'
+import './App.css'
 
 export default function App() {
+    const { t, i18n } = useTranslation()
+
+    const changeLanguage = (language) => {
+        i18n.changeLanguage(language);
+    };
+
     return (
-        <div>
-            Hi Coders!
+        <div className="App">
+            <div className="wrap-btn-languages">
+                <button className="btn-language" onClick={() => changeLanguage("en")}>EN</button>
+                <button className="btn-language" onClick={() => changeLanguage("id")}>ID</button>
+            </div>
+            <hr />
+            <div className="content">
+                <div style={{ fontWeight: 'bold', padding: '10px 0px', fontSize: 'x-large' }}>
+                    {t("title")}
+                </div>
+                <div style={{ fontSize: 'large', fontWeight: 'bold', padding: '10px 0px' }}>
+                    {t("description.part1")}
+                </div>
+                <div style={{ textAlign: 'justify' }}>{t("description.part2")}</div>
+            </div>
         </div>
     )
 }
